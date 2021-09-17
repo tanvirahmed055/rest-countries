695dcb2e96c2aeae929200f4ac1f6885f46671ec
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-09-17 17:30:30 +0600

    added js logics

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+const loadCountries = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+    console.log(countries[0]);
+    const container = document.getElementById('countries');
+    const countryHTML = countries.map(country => getCountryHTML(country));
+    container.innerHTML = countryHTML[0];
+    console.log(countryHTML[0]);
+
+}
+
+const getCountryHTML = country => {
+    return `
+      <div class="country">
+         <h2>${country.name}</h2>
+         <h4>Capital: ${country.capital}</h4>
+         <img src="${country.flag}">
+      </div>   
+    `
+}
+
+loadCountries();
